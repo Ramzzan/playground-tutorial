aaf77a605210c0691b71c95d6cdd921e3969e548
Waleed Ahmed <waleed.wit@gmail.com>
2022-05-06 20:03:45 +0300

    return by value type

--- MyPlayground.playground/Pages/Class-Struct.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Class-Struct.xcplaygroundpage/Contents.swift
@@ -3,16 +3,9 @@
 import Foundation
 
 
-// change type by reference
+// return by value type
 
-/**
-We have an Animal class that only has a name property. Then, we create a dog object and create cat object but assign dog to the cat object.
-
-Since they are reference type objects, they are pointing to the same memory address, so they are actually the same objects!
-If we change one of their property, other one will be affected as well because they are pointing to the same address. Let’s take a look below.
-**/
-
-class Animal {
+struct Animal {
     
     var name: String
     
@@ -31,18 +24,6 @@ print(cat.name) //dog
 
 cat.name = "hound"
 
-print(dog.name) //hound
+print(dog.name) //dog
 print(cat.name) //hound
-
-
-func changeType(type: Animal){
-    type.name = "brid"
-}
-
-
-changeType(type: cat)
-
-print(dog.name) //brid
-print(cat.name) //brid
-
 
